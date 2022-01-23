4e4603628384abc8c24f8412f659a0a055565374
JefferyXie <jex862@usask.ca>
2022-01-23 12:27:09 -0600

    A dictionary for all pokemon soundtracks.

---
+++ client/src/components/Constants/Soundtracks.js
@@ -0,0 +1,67 @@
+const Soundtracks = {
+    "kanto": {
+        0: {name: "Cerulean City", file_path: "soundtrack/kanto/Cerulean City.mp3"},
+        1: {name: "Hiwada Town", file_path: "soundtrack/kanto/Hiwada Town.mp3"},
+        2: {name: "Pokémon Gym", file_path: "soundtrack/kanto/Pokémon Gym.mp3"},
+        3: {name: "Pokémon League", file_path: "soundtrack/kanto/Pokémon League.mp3"},
+        4: {name: "Route 1", file_path: "soundtrack/kanto/Route 1.mp3"},
+        5: {name: "Tanba City", file_path: "soundtrack/kanto/Tanba City.mp3"},
+        6: {name: "Title (HeartGold & SoulSilver)", file_path: "soundtrack/kanto/Title.mp3"}
+    },
+    "johto": {
+        0: {name: "Enju City", file_path: "soundtrack/johto/Enju City.mp3"},
+        1: {name: "Friendly Shop", file_path: "soundtrack/johto/New Bark Town.mp3"},
+        2: {name: "Hall Of Fame", file_path: "soundtrack/johto/Hall Of Fame.mp3"},
+        3: {name: "Indigo Plateau", file_path: "soundtrack/johto/Indigo Plateau.mp3"},
+        4: {name: "New Bark Town", file_path: "soundtrack/johto/New Bark Town.mp3"},
+        5: {name: "Pokemon Center", file_path: "soundtrack/johto/Pokemon Center.mp3"},
+        6: {name: "Rocket Hideout", file_path: "soundtrack/johto/Rocket Hideout.mp3"}
+    },
+    "hoenn": {
+        0: {name: "Kanazumi City", file_path: "soundtrack/hoenn/Kanazumi City.mp3"},
+        1: {name: "Minamo City", file_path: "soundtrack/hoenn/Minamo City.mp3"},
+        2: {name: "Mt. Chimney", file_path: "soundtrack/hoenn/Mt. Chimney.mp3"},
+        3: {name: "Route 101", file_path: "soundtrack/hoenn/Route 101.mp3"},
+        4: {name: "Route 110", file_path: "soundtrack/hoenn/Route 110.mp3"},
+        5: {name: "Shidake Town", file_path: "soundtrack/hoenn/Shidake Town.mp3"},
+        6: {name: "Touka City", file_path: "soundtrack/hoenn/Touka City.mp3"}
+    },
+    "sinnoh": {
+        0: {name: "Kotobuki City (Night)", file_path: "soundtrack/sinnoh/Kotobuki City (Night).mp3"},
+        1: {name: "Kurogane City (Day)", file_path: "soundtrack/sinnoh/Kurogane City (Day).mp3"},
+        2: {name: "Lake", file_path: "soundtrack/sinnoh/Lake.mp3"},
+        3: {name: "Nagisa City (Night)", file_path: "soundtrack/sinnoh/Nagisa City (Night).mp3"},
+        4: {name: "Route 201 (Day)", file_path: "soundtrack/sinnoh/Route 201 (Day).mp3"},
+        5: {name: "Route 205 (Day)", file_path: "soundtrack/sinnoh/Route 205 (Day).mp3"},
+        6: {name: "Route 206 (Night)", file_path: "soundtrack/sinnoh/Route 206 (Night).mp3"}
+    },
+    "unova": {
+        0: {name: "Accumula Town", file_path: "soundtrack/unova/Accumula Town.mp3"},
+        1: {name: "Castelia City", file_path: "soundtrack/unova/Castelia City.mp3"},
+        2: {name: "Nimbasa City", file_path: "soundtrack/unova/Nimbasa City.mp3"},
+        3: {name: "Route 19 (Spring/Summer)", file_path: "soundtrack/unova/Route 19 (Spring & Summer).mp3"},
+        4: {name: "Skyarrow Bridge", file_path: "soundtrack/unova/Skyarrow Bridge.mp3"},
+        5: {name: "Team Plasma Plots", file_path: "soundtrack/unova/Team Plasma Plots.mp3"},
+        6: {name: "The Pokémon League", file_path: "soundtrack/unova/The Pokémon League.mp3"}
+    },
+    "kalos": {
+        0: {name: "Aquacorde Town", file_path: "soundtrack/kalos/Aquacorde Town.mp3"},
+        1: {name: "Cyllage City", file_path: "soundtrack/kalos/Cyllage City.mp3"},
+        2: {name: "Kalos", file_path: "soundtrack/kalos/Kalos.mp3"},
+        3: {name: "Parfum Palace", file_path: "soundtrack/kalos/Parfum Palace.mp3"},
+        4: {name: "Route 1 - Vaniville Pathway", file_path: "soundtrack/kalos/Route 1 - Vaniville Pathway.mp3"},
+        5: {name: "Santalune City", file_path: "soundtrack/kalos/Santalune City.mp3"},
+        6: {name: "Victory Road", file_path: "soundtrack/kalos/Victory Road.mp3"}
+    },
+    "alola": {
+        0: {name: "Festival Plaza (Day)", file_path: "soundtrack/alola/Festival Plaza (Day).mp3"},
+        1: {name: "Hau'oli City (Night)", file_path: "soundtrack/alola/Hau'oli City (Night).mp3"},
+        2: {name: "Heahea City (Day)", file_path: "soundtrack/alola/Heahea City (Day).mp3"},
+        3: {name: "Malie City (Day)", file_path: "soundtrack/alola/Malie City (Day).mp3"},
+        4: {name: "Paniola Town (Day)", file_path: "soundtrack/alola/Paniola Town (Day).mp3"},
+        5: {name: "Route 2 on Melemele Island", file_path: "soundtrack/alola/Route 2 on Melemele Island.mp3"},
+        6: {name: "The Pokémon League", file_path: "soundtrack/alola/The Pokémon League.mp3"}
+    }
+}
+
+export default Soundtracks;
