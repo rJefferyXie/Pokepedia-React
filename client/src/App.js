aad1016f1764b015e57a509ac5038dddce527b97
JefferyXie <jex862@usask.ca>
2022-01-23 12:26:34 -0600

    Created routing using react router.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Hero from './components/Hero/Hero';
+import Navbar from './components/Navbar/Navbar';
+import Pokedex from "./components/Pokedex/Pokedex";
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar></Navbar>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Hero></Hero>}></Route>
+          <Route path="/pokedex" element={<Pokedex></Pokedex>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
