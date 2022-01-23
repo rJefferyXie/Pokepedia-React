884f676a2c57516ba804708c5f9b83bbe8a99a89
JefferyXie <jex862@usask.ca>
2022-01-23 12:30:27 -0600

    Navbar created, need to change colors.

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,9 +1,45 @@
 import "./Navbar.css";
 
 const Navbar = () => {
-  return <div>
 
-  </div>;
-};
+    // Switch to mobile menu
+    const MobileMenu = () => {
+        document.querySelector("#Mobile-menu").classList.toggle('is-active');
+        document.querySelector('#Navbar-container').classList.toggle('active');
+    };
+
+    const RedirectToHome = () => {
+        if (window.location.pathname !== "/") {
+            window.location.pathname = "/";
+        }
+    }
+
+    const ScrollTo = (id) => {
+        RedirectToHome();
+
+        let section = document.querySelector(id).offsetTop - 80;
+        window.scroll({
+            top: section,
+            behavior: "smooth"
+        });    
+    }
+
+    return (
+        <nav id="Navbar" className="flex">
+            <button id="Navbar-logo" onClick={() => ScrollTo("#Hero")}>Poképedia</button>
+            <div id="Mobile-menu" onClick={MobileMenu}>
+                <span className="bar"></span> 
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
+            <div id="Navbar-container" className="flex" onClick={MobileMenu}>
+                <button onClick={() => ScrollTo("#Hero")} className="nav-button">Home</button>
+                <button onClick={() => ScrollTo("#About")} className="nav-button">About</button>
+                <button onClick={() => ScrollTo("#Regions")} className="nav-button">Regions</button>
+                <button onClick={() => ScrollTo("#About")} className="nav-button">Account</button>
+            </div>
+        </nav>
+    );
+}
 
 export default Navbar;
