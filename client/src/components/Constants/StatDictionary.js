8896b1e91b6bf3f42678fceeed9ee098efbe588a
JefferyXie <jex862@usask.ca>
2022-01-23 12:27:39 -0600

    A dictionary for all stats mapped to an index.

---
+++ client/src/components/Constants/StatDictionary.js
@@ -0,0 +1,10 @@
+const StatDictionary = {
+    0: "health",
+    1: "attack",
+    2: "defense",
+    3: "special-attack",
+    4: "special-defense",
+    5: "speed"
+}
+
+export default StatDictionary;
