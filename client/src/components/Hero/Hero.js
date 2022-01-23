fd1466a69f60f6e784da22a7cfa0811b8be442b0
JefferyXie <jex862@usask.ca>
2022-01-23 12:30:55 -0600

    Hero page finished.

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -1,10 +1,27 @@
 import "./Hero.css";
 
+import About from "../About/About";
+import Regions from "../Regions/Regions";
+
+import wallpaper from "../../images/wallpaper.png";
+
+import TypeColorSchemes from "../Constants/TypeColorSchemes";
+import StatDictionary from "../Constants/StatDictionary";
+import Soundtracks from "../Constants/Soundtracks";
+
 const Hero = () => {
   return (
-    <div>
-        
-    </div>
+    <section id="Hero" className="flex-col">
+      <div className="page-container">
+        <img alt="" src={wallpaper} id="Hero-wallpaper"></img>
+        <p id="Hero-text">
+            Welcome to Pokepedia!
+            Revisit your favorite pokemon memories and adventures with us.
+        </p>
+      </div>
+      <About></About>
+      <Regions></Regions>
+    </section>
   );
 };
 
