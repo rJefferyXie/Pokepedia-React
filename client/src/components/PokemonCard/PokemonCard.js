17018a78b34409377d3fe44d1251f988eb8af9fc
JefferyXie <jex862@usask.ca>
2022-01-25 18:46:02 -0600

    Creates a unique card for each pokemon in pokedex.

--- client/src/components/PokemonCard/PokemonCard.js
+++ client/src/components/PokemonCard/PokemonCard.js
@@ -2,52 +2,61 @@ import "./PokemonCard.css";
 import TypeImageMap from "../Constants/TypeImageMap";
 import TypeColorSchemes from "../Constants/TypeColorSchemes"
 
-import { faSearch, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faPlus, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import InspectPage from "../../components/InspectPage/InspectPage";
 
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
-const PokemonCard = (props) => {
+const PokemonCard = ({pokemonData, speciesData, pokedexIndex, addToTeam, removeFromTeam, teamIndex, customStyle}) => {
   const [inspecting, setInspecting] = useState(false);
 
   const inspectPokemon = () => {
     setInspecting(true);
   }
 
-  const addToTeam = () => {
-    console.log("team")
+  const add = () => {
+    addToTeam([pokemonData, speciesData, pokedexIndex]);
+  }
+
+  const remove = () => {
+    removeFromTeam(teamIndex);
   }
 
   const exitInspect = () => {
-    console.log("dwaniuawhfiuahf")
     setInspecting(false);
   }
 
   return (
     inspecting === false ?
-    <div className="pokemon-container flex-col" style={{backgroundColor: TypeColorSchemes[props.pokemonData.types[0].type.name]}}>
+    <div className="pokemon-container flex-col" style={{backgroundColor: TypeColorSchemes[pokemonData.types[0].type.name]}}>
         <div className="pokemon-button-container flex">
-          <button className="pokemon-button-card flex" onClick={addToTeam}>
+          {teamIndex === undefined ?
+          <button className="pokemon-button-card flex" onClick={() => add()}>
             <FontAwesomeIcon style={{margin: "auto"}} icon={faPlus}></FontAwesomeIcon>
           </button>
+          :
+          <button className="pokemon-button-card flex" onClick={() => remove()}>
+            <FontAwesomeIcon style={{margin: "auto"}} icon={faTimes}></FontAwesomeIcon>
+          </button>
+          }
           <button className="pokemon-button-card flex" onClick={inspectPokemon}>
             <FontAwesomeIcon style={{margin: "auto"}} icon={faSearch}></FontAwesomeIcon>
           </button>
         </div>
-        <img className="pokemon-image-card" src={props.pokemonData.sprites.other["official-artwork"].front_default} alt=""></img>
-        <p className="pokemon-index-card">{"#" + props.pokedexIndex}</p>
-        <p className="pokemon-name-card">{props.pokemonData["name"]}</p>
+        <img className="pokemon-image-card" src={pokemonData.sprites.other["official-artwork"].front_default} alt=""></img>
+        <p className="pokemon-index-card">{"#" + pokedexIndex}</p>
+        <p className="pokemon-name-card">{pokemonData["name"]}</p>
 
         <div className="pokemon-type-container-card flex">
-          {props.pokemonData.types.map(type => {
-            return <img className="pokemon-type-card" src={TypeImageMap[type.type.name]} alt={type.type.name}></img>
+          {pokemonData.types.map((type, index) => {
+            return <img className="pokemon-type-card" src={TypeImageMap[type.type.name]} alt={type.type.name} key={index}></img>
           })}
         </div>
     </div>
     :
-    <InspectPage data={props} exit={exitInspect}></InspectPage>
+    <InspectPage pokemonData={pokemonData} speciesData={speciesData} pokedexIndex={pokedexIndex} exit={exitInspect} customStyle={customStyle}></InspectPage>
   );
 };
 
