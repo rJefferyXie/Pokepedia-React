d9c575868c4653414f44ef4ef80591730ac0e979
JefferyXie <jex862@usask.ca>
2022-01-25 18:47:21 -0600

    Hero page responsive.

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -5,14 +5,10 @@ import Regions from "../Regions/Regions";
 
 import wallpaper from "../../images/wallpaper.png";
 
-import TypeColorSchemes from "../Constants/TypeColorSchemes";
-import StatDictionary from "../Constants/StatDictionary";
-import Soundtracks from "../Constants/Soundtracks";
-
 const Hero = () => {
   return (
     <section id="Hero" className="flex-col">
-      <div className="page-container">
+      <div id="Hero-wrapper" className="page-container">
         <img alt="" src={wallpaper} id="Hero-wallpaper"></img>
         <p id="Hero-text">
             Welcome to Pokepedia!
