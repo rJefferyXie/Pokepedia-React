387ec196e6e1844be251adb6a2667dcd3fd0f858
JefferyXie <jex862@usask.ca>
2022-01-29 13:56:04 -0600

    Team and inspect have their own routes.

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
 import Pokedex from "./components/Pokedex/Pokedex";
+import InspectPage from "./components/InspectPage/InspectPage";
+import Team from "./components/Team/Team";
 
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
@@ -14,6 +16,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Hero></Hero>}></Route>
           <Route path="/pokedex" element={<Pokedex></Pokedex>}></Route>
+          <Route path="/inspect" element={<InspectPage></InspectPage>}></Route>
+          <Route path="/team" element={<Team></Team>}></Route>
         </Routes>
       </Router>
     </div>
