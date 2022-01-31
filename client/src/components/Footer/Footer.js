6798fcc8ae3d915447ca3f66c52d4b97d96b9155
JefferyXie <jeffxie27@outlook.com>
2022-01-30 21:04:39 -0600

    Changed class to className.

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,8 +3,8 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <section id="Footer">
-        <p id="github-footer" class="footer-text">Created by <a href="https://github.com/rJefferyXie" class="footer-link" target="_blank">Jeffery Xie</a></p>
-        <p id="pokeapi-footer" class="footer-text">Powered by <a href="https://pokeapi.co/" class="footer-link" target="_blank">Pokeapi</a></p>
+        <p id="github-footer" className="footer-text">Created by <a href="https://github.com/rJefferyXie" className="footer-link" target="_blank" rel="noreferrer">Jeffery Xie</a></p>
+        <p id="pokeapi-footer" className="footer-text">Powered by <a href="https://pokeapi.co/" className="footer-link" target="_blank" rel="noreferrer">Pokeapi</a></p>
     </section>
   );
 };
