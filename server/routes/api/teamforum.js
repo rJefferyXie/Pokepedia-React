73fef1e593b6cffb6d6ef2ca0d3f3d7ef781b05d
JefferyXie <jeffxie27@outlook.com>
2022-02-04 15:12:51 -0600

    Created a new api route for team sharing.

---
+++ server/routes/api/teamforum.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+
+const TeamShare = require("../../models/TeamShare");
+
+// @route GET api/teams/all
+// @desc Get the pokemon pokedex for id
+// @access Public
+router.get("/all", (req, res) => {
+    TeamShare.find({}, (err, data) => {
+        res.json(data);
+    }) 
+});
+
+// @route POST api/teams/create
+// @desc Create a new team
+// @access Public
+router.post("/create", (req, res) => {
+    console.log(req.body);
+    let newTeam = new TeamShare({
+        username: req.body.username,
+        region: req.body.region,
+        team: req.body.team,
+        tags: req.body.tags,
+    });
+    newTeam.save((err, res) => {
+        if (err) { throw err }
+    });
+});
+
+module.exports = router;
