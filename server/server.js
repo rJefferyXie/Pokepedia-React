969197ab28319e20b18992c8dd6d32b2bc0024d5
JefferyXie <jeffxie27@outlook.com>
2022-02-04 15:12:06 -0600

    Added route for team sharing.

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const pokedex = require("./routes/api/pokedex.js");
+const teams = require("./routes/api/teamforum.js");
 
 const db = process.env.ATLAS_URI;
 const port = process.env.PORT || 5000;
@@ -20,5 +21,6 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .catch(err => console.log(err))
 
 app.use("/api/pokedex", pokedex);
+app.use("/api/teams", teams);
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
