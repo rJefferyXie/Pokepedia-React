e068882647573968eb9a9a638dc195b75f67bf97
JefferyXie <jeffxie27@outlook.com>
2022-02-04 15:11:16 -0600

    Pokemon card size changes depending on context.

--- client/src/components/PokemonCard/PokemonCard.js
+++ client/src/components/PokemonCard/PokemonCard.js
@@ -12,7 +12,7 @@ import { Link } from "react-router-dom";
 import { useDispatch } from 'react-redux';
 import allActions from '../../redux/actions/allActions';
 
-const PokemonCard = ({pokemonData, speciesData, pokedexIndex, teamIndex}) => {
+const PokemonCard = ({pokemonData, speciesData, pokedexIndex, teamIndex, dashboard}) => {
   const supportsLazyLoad = ('loading' in document.createElement('img'));
   const { ref, inView } = useInView({
     triggerOnce: true,
@@ -31,30 +31,34 @@ const PokemonCard = ({pokemonData, speciesData, pokedexIndex, teamIndex}) => {
   }
 
   return (
-    <div ref={ref} className="pokemon-container flex-col" style={{backgroundColor: TypeColorSchemes[pokemonData.types[0].type.name]}}>
-        <div className="pokemon-button-container flex">
-          {teamIndex === undefined ?
-          <button className="pokemon-button-card flex" onClick={() => addPokemon()}>
-            <FontAwesomeIcon style={{margin: "auto"}} icon={faPlus}></FontAwesomeIcon>
-          </button>
-          :
-          <button className="pokemon-button-card flex" onClick={() => removePokemon()}>
-            <FontAwesomeIcon style={{margin: "auto"}} icon={faTimes}></FontAwesomeIcon>
-          </button>
-          }
-          <Link to="/inspect" className="pokemon-button-card flex" state={{ pokemonData: pokemonData, speciesData: speciesData, pokedexIndex: pokedexIndex }}>
-            <FontAwesomeIcon style={{margin: "auto"}} icon={faSearch}></FontAwesomeIcon>
-          </Link>
-        </div>
+    <div ref={ref} className={dashboard === undefined ? "pokemon-container flex-col" : "pokemon-container ds flex-col"} style={{backgroundColor: TypeColorSchemes[pokemonData.types[0].type.name]}}>
+        {dashboard === undefined ? 
+          <div className="pokemon-button-container flex">
+            {teamIndex === undefined ?
+            <button className="pokemon-button-card flex" onClick={() => addPokemon()}>
+              <FontAwesomeIcon style={{margin: "auto"}} icon={faPlus}></FontAwesomeIcon>
+            </button>
+            :
+            <button className="pokemon-button-card flex" onClick={() => removePokemon()}>
+              <FontAwesomeIcon style={{margin: "auto"}} icon={faTimes}></FontAwesomeIcon>
+            </button>
+            }
+            <Link to="/inspect" className="pokemon-button-card flex" state={{ pokemonData: pokemonData, speciesData: speciesData, pokedexIndex: pokedexIndex }}>
+              <FontAwesomeIcon style={{margin: "auto"}} icon={faSearch}></FontAwesomeIcon>
+            </Link>
+          </div> : null
+        }
+
         {inView || supportsLazyLoad ? <img className="pokemon-image-card" src={pokemonData.sprites.other["official-artwork"].front_default} alt="" loading="lazy"></img> : null}
-        <p className="pokemon-index-card">{"#" + pokedexIndex}</p>
+        {pokedexIndex !== undefined ? <p className="pokemon-index-card">{"#" + pokedexIndex}</p>: null}
         <p className="pokemon-name-card searchable">{pokemonData["name"]}</p>
 
-        <div className="pokemon-type-container-card searchable flex">
+        {dashboard === undefined ? <div className="pokemon-type-container-card searchable flex">
           {pokemonData.types.map((type, index) => {
             return <img className="pokemon-type-card" src={TypeImageMap[type.type.name]} alt={type.type.name} key={index}></img>
           })}
         </div>
+        : null}
     </div>
   );
 };
