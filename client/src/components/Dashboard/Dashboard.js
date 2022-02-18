ff4aebf76f4095337abd74907d920f9a8efe6922
JefferyXie <jeffxie27@outlook.com>
2022-02-18 16:35:10 -0600

    Designed dashboard, added pagination tool.

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -2,6 +2,7 @@ import "./Dashboard.css";
 
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
+import Pagination from '@mui/material/Pagination';
 
 import PokemonCard from "../PokemonCard/PokemonCard";
 
@@ -15,25 +16,28 @@ const Dashboard = () => {
 
   return (
     <section id="Dashboard-container" className="flex">
-      {posts.map((post, index) => {
-      return <div className="team-post flex-col" key={index}>
-        <div className="team-titles flex">
-          <h3>{post.username}</h3>
-          <h3 style={{textTransform: "capitalize"}}>{post.region}</h3>
+      <div id="Dashboard-right" className="flex">
+        {posts.map((post, index) => {
+        return <div className="team-post flex-col" key={index}>
+          <div className="team-titles flex">
+            <h3>{post.username}</h3>
+            <h3 style={{textTransform: "capitalize"}}>{post.region}</h3>
+          </div>
+          <div className="team-wrapper flex">
+            {post.team.map((pokemon, teamIndex) => {
+              return <PokemonCard pokemonData={pokemon.pokemonData} speciesData={pokemon.speciesData} dashboard={true} key={teamIndex}></PokemonCard>
+            })}
+          </div>
+          <div className="team-tags flex">
+            Tags: 
+            {post.tags.map((tag, tagIndex) => {
+              return <p className="team-tag" key={tagIndex}>{tag}</p>
+            })}
+          </div>
         </div>
-        <div className="team-wrapper flex">
-          {post.team.map((pokemon, teamIndex) => {
-            return <PokemonCard pokemonData={pokemon.pokemonData} speciesData={pokemon.speciesData} dashboard={true} key={teamIndex}></PokemonCard>
-          })}
-        </div>
-        <div className="team-tags flex">
-          Tags: 
-          {post.tags.map((tag, tagIndex) => {
-            return <p className="team-tag" key={tagIndex}>{tag}</p>
-          })}
-        </div>
+        })}
+        <Pagination count={10} variant="outlined" color="primary" style={{margin: "auto"}}></Pagination>
       </div>
-      })}
     </section>
   );
 };
