4c76c6ecb2c89d2d7473ff2cc3476e3e86ddbdd1
JefferyXie <jeffxie27@outlook.com>
2022-02-18 16:35:20 -0600

    Moved hero text off of image.

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -8,12 +8,12 @@ import wallpaper from "../../images/wallpaper.png";
 const Hero = () => {
   return (
     <section id="Hero" className="flex-col">
-      <div id="Hero-wrapper" className="page-container">
-        <img alt="" src={wallpaper} id="Hero-wallpaper"></img>
-        <p id="Hero-text">
+      <div id="Hero-wrapper" className="page-container flex-col">
+        <h3 id="Hero-text">
             Welcome to Pokepedia!
             Revisit your favorite pokemon memories and adventures with us.
-        </p>
+        </h3>
+        <img alt="" src={wallpaper} id="Hero-wallpaper"></img>
       </div>
       <About></About>
       <Regions></Regions>
