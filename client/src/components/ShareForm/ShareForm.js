c00243d2636c1886fdfe237c21c5be06a6072700
JefferyXie <jeffxie27@outlook.com>
2022-02-18 16:36:19 -0600

    Share Form remade.

--- client/src/components/ShareForm/ShareForm.js
+++ client/src/components/ShareForm/ShareForm.js
@@ -6,6 +6,9 @@ import axios from "axios";
 import { useState } from "react";
 import { useSelector } from 'react-redux';
 
+import { ClickAwayListener, Button } from "@mui/material";
+
+
 const ShareForm = ({cancel}) => {
     const pokemonTeam = useSelector(state => state.teamReducer.team);
     const pokedex = useSelector(state => state.pokedexReducer);
@@ -37,33 +40,39 @@ const ShareForm = ({cancel}) => {
         }
     }
 
-    return <div id="Share-form" className="flex-col">
-        <input id="Username" type="text" placeholder="Enter a name..." maxLength="20"></input>
-        <div id="Share-team" className="flex team-wrapper">
-          {[...Array(6)].map((_, i) => {
-            if (pokemonTeam[i] === undefined) { return <div className="pokemon-container ds flex" key={i}></div> }
-            return <PokemonCard pokemonData={pokemonTeam[i].pokemonData} speciesData={pokemonTeam[i].speciesData} dashboard={true} key={i}></PokemonCard>
-          })}
-        </div>   
-        <div id="Share-tags" className="flex-col">
-            <h3>Select up to 2 tags.</h3>
-            <div className="team-tags flex">
-                <button id="tag1" className="team-tag" onClick={() => setSelected("tag1")}>Try to beat me!</button>
-                <button id="tag2" className="team-tag" onClick={() => setSelected("tag2")}>So cool!</button>
-                <button id="tag3" className="team-tag" onClick={() => setSelected("tag3")}>Get your game on!</button>
-                <button id="tag4" className="team-tag" onClick={() => setSelected("tag4")}>The best!</button>
-                <button id="tag5" className="team-tag" onClick={() => setSelected("tag5")}>Best Team!</button>
-                <button id="tag6" className="team-tag" onClick={() => setSelected("tag6")}>Badge Collector!</button>
-                <button id="tag7" className="team-tag" onClick={() => setSelected("tag7")}>Never Give Up!</button>
-                <button id="tag8" className="team-tag" onClick={() => setSelected("tag8")}>Super Team!</button>
-                <button id="tag9" className="team-tag" onClick={() => setSelected("tag9")}>Legendary!</button>
-            </div>
+    return (
+        <div id="Overlay">
+            <ClickAwayListener onClickAway={() => cancel()}>
+                <div id="Share-form" className="flex-col">
+                    <input id="Username" type="text" placeholder="Enter a name..." maxLength="20"></input>
+                    <div id="Share-team" className="flex team-wrapper">
+                    {[...Array(6)].map((_, i) => {
+                        if (pokemonTeam[i] === undefined) { return <div className="pokemon-container ds flex" key={i}></div> }
+                        return <PokemonCard pokemonData={pokemonTeam[i].pokemonData} speciesData={pokemonTeam[i].speciesData} dashboard={true} key={i}></PokemonCard>
+                    })}
+                    </div>   
+                    <div id="Share-tags" className="flex-col">
+                        <h3 style={{margin: "4px auto"}}>Select up to 2 tags.</h3>
+                        <div className="team-tags flex">
+                            <button id="tag1" className="team-tag" onClick={() => setSelected("tag1")}>Try to beat me!</button>
+                            <button id="tag2" className="team-tag" onClick={() => setSelected("tag2")}>So cool!</button>
+                            <button id="tag3" className="team-tag" onClick={() => setSelected("tag3")}>Get your game on!</button>
+                            <button id="tag4" className="team-tag" onClick={() => setSelected("tag4")}>The best!</button>
+                            <button id="tag5" className="team-tag" onClick={() => setSelected("tag5")}>Best Team!</button>
+                            <button id="tag6" className="team-tag" onClick={() => setSelected("tag6")}>Badge Collector!</button>
+                            <button id="tag7" className="team-tag" onClick={() => setSelected("tag7")}>Never Give Up!</button>
+                            <button id="tag8" className="team-tag" onClick={() => setSelected("tag8")}>Super Team!</button>
+                            <button id="tag9" className="team-tag" onClick={() => setSelected("tag9")}>Legendary!</button>
+                        </div>
+                    </div>
+                    <div className="button-container flex">
+                        <Button variant="contained" color="error" onClick={() => cancel()} style={{margin: "auto 4px", width: "fit-content"}}>Cancel</Button>
+                        <Button variant="contained" color="success" onClick={() => shareTeam()} style={{margin: "auto 4px", width: "fit-content"}}>Share</Button>
+                    </div>
+                </div>
+            </ClickAwayListener>
         </div>
-        <div className="button-container flex">
-            <button className="text-button team-button" style={{backgroundColor: "#F77F7F"}} onClick={() => cancel()}>Cancel</button>
-            <button className="text-button team-button" style={{backgroundColor: "#A7DB8D"}} onClick={() => shareTeam()}>Share</button>
-        </div>
-    </div>;
+    );
 };
 
 export default ShareForm;
