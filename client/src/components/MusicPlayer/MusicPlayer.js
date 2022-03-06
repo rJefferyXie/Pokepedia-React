cfd279e72ad1c2841e25f030101b8cf1b7cffa0c
JefferyXie <jeffxie27@outlook.com>
2022-03-05 21:59:26 -0600

    Changed music note color.

--- client/src/components/MusicPlayer/MusicPlayer.js
+++ client/src/components/MusicPlayer/MusicPlayer.js
@@ -110,7 +110,7 @@ const MusicPlayer = () => {
                     <CloseIcon style={{padding: "0px 0px 0px 0px", width: "16px", height: "16px", color: "red"}}></CloseIcon>
                 </IconButton> :                 
                 <IconButton onClick={() => dispatch(allActions.musicActions.open())}>
-                    <MusicNoteIcon style={{padding: "0px 0px 0px 0px", width: "16px", height: "16px", color: "green"}}></MusicNoteIcon>
+                    <MusicNoteIcon style={{padding: "0px 0px 0px 0px", width: "16px", height: "16px"}}></MusicNoteIcon>
                 </IconButton>}
             </Tooltip>
             {music.closed === false ? <Typography style={{padding: "0px 8px 0px 8px", margin: "auto", width: "160px", height: "20px", overflow: "hidden", textOverflow: "ellipsis", textAlign: "center"}}>{Soundtracks[regions[regionNumber]][trackNumber]["name"]}</Typography> : null}
