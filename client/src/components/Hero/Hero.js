796240652277075c47be1bdc8ecd0efe6e4fabc2
JefferyXie <jeffxie27@outlook.com>
2022-03-05 21:58:58 -0600

    Changed hero layout from column to row.

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -7,7 +7,14 @@ import wallpaper from "../../images/wallpaper.png";
 import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
 
+import React, { useEffect } from "react";
+import { useDispatch } from 'react-redux';
+import allActions from '../../redux/actions/allActions';
+
+
 const Hero = () => {
+  const dispatch = useDispatch()
+
   const scrollToRegions = () => {
       let section = document.querySelector("#Regions").offsetTop - 100;
       window.scroll({
@@ -16,18 +23,22 @@ const Hero = () => {
       });  
   }
 
+  useEffect(() => {
+    dispatch(allActions.loadActions.setLoaded(false));
+  }, []);
+
   return (
     <section id="Hero" className="flex-col">
-      <div id="Hero-wrapper" className="page-container flex-col">
-        <h3 id="Hero-text">
-            Welcome to Pokepedia!
-            Revisit your favorite pokemon memories and adventures with us.
-        </h3>
-        <img alt="" src={wallpaper} id="Hero-wallpaper"></img>
-        <div id="Hero-button-container" className="flex">
-          <Button variant="contained" onClick={() => scrollToRegions()} style={{width: "fit-content", margin: "0px 4px"}}>Select a Region</Button>
-          <Button variant="contained" component={Link} to="/dashboard" style={{width: "fit-content", margin: "0px 4px"}}>View Dashboard</Button>
+      <div id="Hero-wrapper" className="page-container flex">
+        <div id="Left-hero-wrapper" className="flex-col">
+          <h1 style={{margin: "auto", width: "100%", textAlign: "center"}}>Welcome to Pokepedia!</h1>
+          <p style={{margin: "0px auto 20px", width: "100%", textAlign: "center"}}>Revisit your favorite pokemon memories and adventures with us.</p>
+          <div id="Hero-button-container" className="flex">
+            <Button variant="contained" className="mui-button" onClick={() => scrollToRegions()} style={{width: "fit-content", margin: "0px 4px", backgroundColor: "rgba(6, 114, 177, 0.8)"}}>Select a Region</Button>
+            <Button variant="contained" className="mui-button" component={Link} to="/dashboard" style={{width: "fit-content", margin: "0px 4px", backgroundColor: "rgba(6, 114, 177, 0.8)"}}>View Dashboard</Button>
+          </div>
         </div>
+        <img alt="" src={wallpaper} id="Hero-wallpaper"></img>
       </div>
       <About></About>
       <Regions></Regions>
